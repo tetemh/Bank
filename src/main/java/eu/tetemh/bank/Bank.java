80103ae68527830beb027045840398939ce20917
Téo <76852029+tetemh@users.noreply.github.com>
2022-01-20 22:30:01 +0100

    init

---
+++ src/main/java/eu/tetemh/bank/Bank.java
@@ -0,0 +1,17 @@
+package eu.tetemh.bank;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Bank extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
